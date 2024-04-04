7fcb42de624fa656fda9e6c11a15239e8acbfee9
giapmanne69 <giap200496@gmail.com>
2024-04-04 15:52:43 +0700

    Them file C#

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string a = Console.ReadLine();
+      Console.WriteLine(a);
+    }
+  }
+}
