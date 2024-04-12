e53f57b9b75aa02b42037b51045a6a90070f5848
giapmanne69 <giap200496@gmail.com>
2024-04-12 13:43:02 +0700

    CÁU TRÚC DỮ LIỆU HÀNG ĐỢI 2

---
+++ CẤU TRÚC DỮ LIỆU HÀNG ĐỢI 2.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+int main()
+{
+    queue<int> q;
+    int t; cin >> t;
+    while (t--){
+        string s; cin >> s;
+        if (s=="PUSH"){
+            int n; cin >> n;
+            q.push(n);
+        }
+        if (s=="POP"){
+            if (!q.empty()) q.pop();
+        }
+        if (s=="PRINTFRONT"){
+            if (q.empty()) cout << "NONE\n";
+            else cout << q.front() << "\n";
+        }
+    }
+}
