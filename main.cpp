a2db633a5060944e6452c27e23e0dac96891f1cf
giapmanne69 <giap200496@gmail.com>
2024-04-12 13:39:18 +0700

    HÀNG ĐỢI HAI ĐẦU (DEQUEUE)

--- main.cpp
+++ main.cpp
@@ -1,22 +1,33 @@
 #include<iostream>
-#include<queue>
+#include<deque>
 using namespace std;
 int main()
 {
-    queue<int> q;
+    deque<int> dq;
     int t; cin >> t;
     while (t--){
         string s; cin >> s;
-        if (s=="PUSH"){
-            int n; cin >> n;
-            q.push(n);
-        }
-        if (s=="POP"){
-            if (!q.empty()) q.pop();
+        if (s=="PUSHFRONT"){
+            int x; cin >> x;
+            dq.push_front(x);
         }
         if (s=="PRINTFRONT"){
-            if (q.empty()) cout << "NONE\n";
-            else cout << q.front() << "\n";
+            if (dq.empty()) cout << "NONE\n";
+            else cout << dq.front() << "\n";
+        }
+        if (s=="POPFRONT"){
+            if (!dq.empty()) dq.pop_front();
+        }
+        if (s=="PUSHBACK"){
+            int n; cin >> n;
+            dq.push_back(n);
+        }
+        if (s=="PRINTBACK"){
+            if (dq.empty()) cout << "NONE\n";
+            else cout << dq.back() << "\n";
+        }
+        if (s=="POPBACK"){
+            if (!dq.empty()) dq.pop_back();
         }
     }
 }
